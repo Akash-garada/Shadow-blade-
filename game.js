6572a99574f52cd5ae9ab980b0de5fdf154cf1dd
Akash-garada <onlinelozy@gmail.com>
2026-02-12 20:49:20 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
-class MainScene extends Phaser.Scene {
+
+        class MainScene extends Phaser.Scene {
     constructor() {
         super("main");
+        this.health = 3;
+        this.canDoubleJump = false;
+        this.gameOver = false;
     }
 
     preload() {
@@ -12,31 +16,42 @@ class MainScene extends Phaser.Scene {
 
     create() {
 
+        // Background
         this.add.image(640, 360, 'sky').setScale(2);
 
+        // Platforms
         this.platforms = this.physics.add.staticGroup();
         this.platforms.create(640, 700, 'ground').setScale(4).refreshBody();
 
+        // Player
         this.player = this.physics.add.sprite(200, 500, 'player');
-        this.player.setBounce(0.1);
         this.player.setCollideWorldBounds(true);
-
         this.physics.add.collider(this.player, this.platforms);
 
-        this.enemy = this.physics.add.sprite(900, 500, 'enemy');
-        this.enemy.setBounce(1);
-        this.enemy.setCollideWorldBounds(true);
-        this.enemy.setVelocityX(-100);
+        // Enemy Group
+        this.enemies = this.physics.add.group();
 
-        this.physics.add.collider(this.enemy, this.platforms);
+        let enemy1 = this.enemies.create(900, 500, 'enemy');
+        enemy1.setVelocityX(-100);
+        enemy1.setCollideWorldBounds(true);
+        enemy1.setBounce(1);
 
-        this.physics.add.overlap(this.player, this.enemy, this.hitEnemy, null, this);
+        this.physics.add.collider(this.enemies, this.platforms);
+        this.physics.add.overlap(this.player, this.enemies, this.damagePlayer, null, this);
 
+        // Controls
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.attackKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+
+        // UI
+        this.healthText = this.add.text(20, 20, 'Health: 3', { fontSize: '32px', fill: '#fff' });
     }
 
     update() {
 
+        if (this.gameOver) return;
+
+        // Movement
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-200);
         }
@@ -47,13 +62,43 @@ class MainScene extends Phaser.Scene {
             this.player.setVelocityX(0);
         }
 
-        if (this.cursors.up.isDown && this.player.body.touching.down) {
-            this.player.setVelocityY(-500);
+        // Jump + Double Jump
+        if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+            if (this.player.body.touching.down) {
+                this.player.setVelocityY(-500);
+                this.canDoubleJump = true;
+            } 
+            else if (this.canDoubleJump) {
+                this.player.setVelocityY(-500);
+                this.canDoubleJump = false;
+            }
+        }
+
+        // Attack
+        if (Phaser.Input.Keyboard.JustDown(this.attackKey)) {
+            this.enemies.children.iterate((enemy) => {
+                if (enemy && Phaser.Math.Distance.Between(this.player.x, this.player.y, enemy.x, enemy.y) < 100) {
+                    enemy.destroy();
+                }
+            });
         }
     }
 
-    hitEnemy(player, enemy) {
-        this.scene.restart();
+    damagePlayer(player, enemy) {
+
+        this.health -= 1;
+        this.healthText.setText('Health: ' + this.health);
+
+        player.setTint(0xff0000);
+
+        this.time.delayedCall(200, () => {
+            player.clearTint();
+        });
+
+        if (this.health <= 0) {
+            this.gameOver = true;
+            this.add.text(500, 300, 'GAME OVER', { fontSize: '64px', fill: '#ff0000' });
+        }
     }
 }
 
